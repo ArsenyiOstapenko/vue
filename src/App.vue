23bb2e9a7c1d9a1338c3293c21e824a121febf15
ArsenyiOstapenko <43719241+ArsenyiOstapenko@users.noreply.github.com>
2020-12-11 11:37:43 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="app">
+        <h1>Конвертер</h1>
+        <div class="line">
+            <Converter currA="USD" currB="BRL"></Converter>
+            <Converter currA="BRL" currB="USD"></Converter>
+        </div>
+        <div class="line">
+            <Converter currA="CAD" currB="BRL"></Converter>
+            <Converter currA="BRL" currB="CAD"></Converter>
+        </div>
+        <div class="line">
+            <Converter currA="EUR" currB="BRL"></Converter>
+            <Converter currA="BRL" currB="EUR"></Converter>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+
+    import Converter from "./components/Converter.vue";
+
+    export default {
+        name: 'app',
+        components: {
+            Converter
+        }
+    };
+</script>
+
+<style>
+     body{
+       background-image: url("18Pe.gif");
+     }
+
+    #app {
+        max-width: 900px;
+        margin: auto;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #DC143C;
+        margin-top: 60px;
+
+
+    }
+
+    .line {
+        margin-top: 40px;
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .footerStyle {
+        margin-top: 20px;
+        text-align: right;
+        font-size: 9px;
+    }
+
+
+</style>
---
+++ src/components/Converter.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="converter">
+    <h2>{{currA}} to {{currB}}</h2>
+    <input type="text" v-model="currA_value" v-bind:placeholder="currA">
+    <input type="button" value="Convert" v-on:click="converter">
+    <h2>{{currB_value}}</h2>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Converter",
+  props: ["currA", "currB"],
+  data() {
+    return {
+      currA_value: "",
+      currB_value: 0
+    };
+  },  
+  methods: {
+    converter() {
+      let from_to = this.currA + "_" + this.currB;
+      let cA = this.currA;
+      let cB = this.currB;
+      let url =
+        "https://free.currconv.com/api/v7/convert?q=" +
+        from_to +
+        "&compact=ultra&apiKey=ce3469123d58a045204c";
+      //fetch url
+      fetch(url)
+        .then(res =>{
+          return res.json();
+        })
+        .then(json => {          
+          let cot = json[from_to];    
+          console.log("Current value from 1 "+cA+" to 1 "+cB+" : " + cot);      
+          this.currB_value = (cot * parseFloat(this.currA_value)).toFixed(2);
+        })
+        .catch(err => console.error(err));
+    }
+  }
+};
+</script>
+
+<style scoped>
+.converter{
+  padding: 20px;
+  max-width: 300px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+}
+
+</style>
